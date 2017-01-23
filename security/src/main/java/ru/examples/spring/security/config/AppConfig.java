0484247053c3913f66706fbe8ebcd443ae1c51e4
drno-reg <drno-reg@mail.ru>
2017-01-23 12:58:09 +0300

    Первая подготовка проекта

---
+++ security/src/main/java/ru/examples/spring/security/config/AppConfig.java
@@ -0,0 +1,27 @@
+package ru.examples.spring.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "ru.examples.spring.security.web.*" })
+@Import({ SecurityConfig.class })
+public class AppConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/pages/");
+      //  viewResolver.setPrefix("/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
