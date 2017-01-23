e0ddaa1aeae305faa6e3400a7c61f83c4e42044e
drno-reg <drno-reg@mail.ru>
2017-01-23 13:18:08 +0300

    Первая подготовка проекта

--- security/src/main/java/ru/examples/spring/security/config/AppConfig.java
+++ security/src/main/java/ru/examples/spring/security/config/AppConfig.java
@@ -18,8 +18,8 @@ public class AppConfig {
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/pages/");
-      //  viewResolver.setPrefix("/");
+     //   viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setPrefix("/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
